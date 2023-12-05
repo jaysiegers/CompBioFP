bb9f8e8ad07a7f22081ba115d6d0fcaedf33483e
jayisnotgay <91600143+jayisnotgay@users.noreply.github.com>
2023-12-05 16:04:25 +0700

    upload dataset

--- prediction_model.py
+++ prediction_model.py
@@ -1,25 +1,19 @@
-# Import necessary libraries
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, models
 
 # Load your protein structure dataset (features and labels)
-# This dataset should include amino acid sequences and corresponding protein structures (e.g., secondary structure information)
-
+dataset = pd.read_csv("raw_dataset/2022-12-17-pdb-intersect-pisces_pc30_r2.5")
+dataset.head()
 # Preprocess the data
-# Convert amino acid sequences to numerical representations (one-hot encoding)
-# Normalize and preprocess protein structure labels
-
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
-# Build a simple deep learning model using TensorFlow/Keras
+# Build a simple RNN model using TensorFlow/Keras
 model = models.Sequential([
     layers.Embedding(input_dim=num_amino_acids, output_dim=embedding_dim, input_length=max_sequence_length),
-    layers.Conv1D(128, kernel_size=3, activation='relu'),
-    layers.MaxPooling1D(pool_size=2),
-    layers.Flatten(),
+    layers.SimpleRNN(128, activation='relu'),
     layers.Dense(64, activation='relu'),
     layers.Dense(num_classes, activation='softmax')
 ])
@@ -34,8 +28,5 @@ model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
 test_loss, test_accuracy = model.evaluate(X_test, y_test)
 print(f'Test Accuracy: {test_accuracy}')
 
-# Make predictions on new data
-# predictions = model.predict(new_data)
-
 # Save the trained model
-model.save('protein_structure_prediction_model.h5')
+model.save('protein_structure_prediction_rnn_model.h5')
